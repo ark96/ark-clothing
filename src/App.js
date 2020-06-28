9e16c8a9cd206d18918a06cacadf0ded015ad5ec
Abhnav Reddy <abhinavreddy0204@gmail.com>
2020-06-28 14:12:54 +0530

    added routin

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component.jsx';
+import {Route} from 'react-router-dom';
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+)
 
 function App() {
   return (
     <div >
-      <HomePage />
+      <switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/Hats' component={HatsPage} />
+      </switch>
     </div>
   );
 }
